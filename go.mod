4baa76d0c4e98db46c8cbc1f32d4078ae6fcf842
relt <haxrelt@gmail.com>
2023-12-18 19:25:31 +0300

    /cars POST endpoint for creating a cars

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/tehrelt/volkswagen-reference-api
 go 1.21.3
 
 require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.19 // indirect
--- internal/api/server.go
+++ internal/api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log/slog"
 	"net/http"
 	"os"
@@ -8,6 +10,7 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/tehrelt/volkswagen-reference-api/internal/middleware"
+	"github.com/tehrelt/volkswagen-reference-api/internal/models"
 	"github.com/tehrelt/volkswagen-reference-api/internal/repository"
 )
 
@@ -39,4 +42,51 @@ func (s *server) configureRouter() {
 	s.router.Use(middleware.LogMiddleware(s.logger))
 	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
 
+	car := s.router.PathPrefix("/cars").Subrouter()
+	car.HandleFunc("/", s.handleCreateCar()).Methods("POST")
+}
+
+func (s *server) handleCreateCar() http.HandlerFunc {
+	type request struct {
+		Model       string `json:"model"`
+		ReleaseYear int    `json:"release_year"`
+		Description string `json:"description,omitempty"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.logger.Debug("cant decode a body of request", slog.Any("err", err))
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		c := &models.Car{
+			Model:       req.Model,
+			ReleaseYear: req.ReleaseYear,
+			Description: sql.NullString{
+				String: req.Description,
+			},
+		}
+
+		if err := s.store.Car().Create(c); err != nil {
+			s.logger.Debug("cant create an alias", slog.Any("err", err))
+			s.error(w, r, http.StatusUnprocessableEntity, err)
+			return
+		}
+
+		s.respond(w, r, http.StatusOK, c)
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		json.NewEncoder(w).Encode(data)
+	}
 }
